1ece727d09b33753c39dde191015519e1721b299
Israel Martins <israelmartins92@outlook.com>
2024-11-14 12:03:35 -0300

    Modificando

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,18 +2,23 @@ import Tenis from "../assets/images/tenis.png";
 function ProductCard(props) {
     return (
         <>
-        <div className="card">
-            <div className="h-[321px] relative w-[292px] bg-[white]">
-            <div className="absolute w-[96px] top-[20px] left-[20px] rounded-[29px] bg-[yellow] text-center py-[4px] px-[8px]">30% OFF</div>
+        <div className="bg-white-300 flex flex-col">
+          <div className="w-[220px] bg-[#F9F8FE] rounded border drop-shadow-[6px_16px_30px_#6962620D] text-[#474747]">
+          <div className="absolute w-[96px] h-[32px] bg-[#E7FF86] rounded-[29px] mt-3 ml-5 text-center text-sm font-bold leading-[31px]">30% OFF</div> 
+                
                 <img  src={Tenis}  className=""/>
-            </div>
-            <div className="w-[292px] mt-[17px]">
-                <div className="w-[292px] text-[14px]">Tênis</div>
-                <span className="w-[292px] text-[28px]">K-Swiss V8 - Masculino</span>
-                <div className="w-[292px] text-[22px]">
-                    <span className="line-through text[]">$200</span>
-                    <span className="ml-[5px]">$100</span>
+                <div className="flex flex-col gap-1">
+                
+                <span className="text-xs text-[#8F8F8F] ">Tênis</span>
+                <span className="text-2xl font-normal leading-[38px]">{props.name}</span>
+                <div className="flex gap-2 ">
+                  <span className="line-through">{props.price}</span>
+                  <span className="font-bold">{props.priceDiscount}</span>
+                  <p className="text-[20px] font-[400] leading-[38px]">K-Swiss V8 - Masculino</p>
+                  <p className="text-[24px] font-[700] leading-[38px]"><s class="text-[#8f8f8f] text-[24px] font-[400] leading-[38px]">R$200</s> R$100</p>
                 </div>
+              </div>
+            
             </div>
         </div>
         </>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,26 @@
 import CardSale from "../components/CardSale";
 import Gallery from "../components/Gallery"
 import Layout from "../components/Layout";
-import ProductCard from "../components/ProductCard";
+import ProductListing from "../components/ProductListing";
+import Section from "../components/Section";
 
 export default function HomePage() {
     return (
         <Layout>
-
         <Gallery />
+        <Section>
         <CardSale />
-        <ProductCard />
+        </Section>
+        <Section title ="Produtos em alta"
+        titleAlign = "left"
+        link ={{
+            text: "ver todos",
+            href: "https://redirect.link",
+            color: "#C92071"
+        }}>
+            <ProductListing maxNumber='8' />
+        </Section>
+
         
         </Layout>
 
